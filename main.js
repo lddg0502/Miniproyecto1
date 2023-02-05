8dc0e1dd48120da17aa6b7210fd8702ac2bb0d91
lddg0502 <ldomingos@correo.unimet.edu.ve>
2023-02-05 15:59:08 -0400

    skill
    Aun no esta terminado

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 var ind = 1;
+const skill = document.getElementsByClassName("skill")
+const skills = [
+    {nombre: "Curso de Udemy", habilidades: "Css, javascript, html, php", estado:"completado"},
+    {nombre: "Curso de Udemy2", habilidades: "sql", estado: "en progreso"},
+    {nombre: "Curso de la UCV", habilidades: "Java", estado: "completado"},
+]
 mostrarCarrusel(ind);
+addskills();
 
 function plusSlides(n) {
     mostrarCarrusel(ind += n);
@@ -19,3 +26,27 @@ function mostrarCarrusel(n) {
   }
   carrusel[ind-1].style.display = "block"; 
 }
+
+
+
+function addskills(){
+    for (i = 0; i<skills.length; i++) {
+        let Css = "skill"
+        if (skills.estado == "en progreso"){
+            Css += "_incompleto"
+        }else {
+            Css += "_completo"
+        }
+        console.log(Css)
+        const skillHtml = `
+        <div class="skill">
+        <h2 class="${Css}"> ${skills[i].nombre} </h2>
+        <h3 class="${Css}"> ${skills[i].habilidades} </h3>
+        </div>`
+    skills.innerHTML += skillHtml
+    }
+   
+}
+
+    
+    
